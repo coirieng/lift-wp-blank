5b236d3b9c99f550b812ca88c4c54138ed5d206e
Nguyen Pham <baonguyenyam@gmail.com>
2021-06-30 09:36:25 -0500

    add composer

--- wp-content/themes/lift-theme/tasks/browser-sync.js
+++ wp-content/themes/lift-theme/tasks/browser-sync.js
@@ -1,8 +1,14 @@
 module.exports = function (gulp, $, browserSync) {
 	gulp.task('browser-sync', function () {
 		browserSync.init({
-			server: {
-				baseDir: "./dist"
+			// SERVER  
+			// server: {
+			// 	baseDir: "./dist"
+			// },
+			// LOCALHOST DEV 
+			proxy: {
+				target: "http://demo.local:8888",
+				ws: true
 			}
 		});
 	});
