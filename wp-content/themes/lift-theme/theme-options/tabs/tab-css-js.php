141fdd67ac1ccf817f0d01252797a1cd1911fda5
Nguyen Pham <baonguyenyam@gmail.com>
2021-07-03 21:52:11 -0500

    CSS

--- wp-content/themes/lift-theme/theme-options/tabs/tab-css-js.php
+++ wp-content/themes/lift-theme/theme-options/tabs/tab-css-js.php
@@ -17,12 +17,11 @@
 		'title'      => __( 'SCSS/SASS', 'lift-theme-options' ),
 		'id'         => 'lift-theme-cssjs-scss',
         'subsection' => true,
+		'desc' => __('Sass is a stylesheet language that’s compiled to CSS. It allows you to use variables, nested rules, mixins, functions, and more, all with a fully CSS-compatible syntax. Sass helps keep large stylesheets well-organized and makes it easy to share design within and across projects.', 'lift-theme-options'),
 		'fields'     => array(
 			array(
 				'id'       => 'lift-theme-cssjs-scss-code',
                 'type'     => 'ace_editor',
-				'title'    => __('CSS Compiler', 'lift-theme-options'),
-				'subtitle' => __('Sass is a stylesheet language that’s compiled to CSS. It allows you to use variables, nested rules, mixins, functions, and more, all with a fully CSS-compatible syntax. Sass helps keep large stylesheets well-organized and makes it easy to share design within and across projects.', 'lift-theme-options'),
 				'mode'     => 'scss',
 				'class' => 'lift-theme-admin-cssjs',
 				'theme'    => 'monokai',
@@ -32,18 +31,25 @@
 					'maxLines' => 100
 				)
 			),
+			array(
+				'id'    => 'info_sass',
+				'type'  => 'info',
+				'title' => __('Sass Basics', 'lift-theme-options'),
+				'style' => 'success',
+				'icon' => 'bi bi-info',
+				'desc'       => __( 'If you want to learn how to get everything setup, go here: ', 'lift-theme-options' ) . '<a href="//sass-lang.com/guide" target="_blank">sass-lang.com/guide</a>',
+			),
 		),
     ) );
 	Redux::setSection( $opt_name, array(
         'title'      => __( 'CSS', 'lift-theme-options' ),
         'id'         => 'lift-theme-cssjs-css',
         'subsection' => true,
+		'desc'		=> __( 'If you have any custom CSS you would like added to the site, please enter it here.', 'lift-theme-options' ),
 		'fields'     => array(
 			array(
 				'id'       => 'lift-theme-cssjs-css-code',
                 'type'     => 'ace_editor',
-				'title'    => __('CSS Code', 'lift-theme-options'),
-				'subtitle'		=> __( 'If you have any custom CSS you would like added to the site, please enter it here.', 'lift-theme-options' ),
 				'mode'     => 'css',
 				'class' => 'lift-theme-admin-cssjs',
 				'theme'    => 'monokai',
@@ -60,12 +66,11 @@
         'title'      => __( 'JavaScript/Babel', 'lift-theme-options' ),
         'id'         => 'lift-theme-cssjs-js',
         'subsection' => true,
+		'desc'		=> __( 'Please enter in any custom javascript you wish to add to the head of your pages. Requires opening and closing script tags.', 'lift-theme-options' ),
 		'fields'     => array(
 			array(
 				'id'       => 'lift-theme-cssjs-js-code',
                 'type'     => 'ace_editor',
-				'title'    => __('JS Code', 'lift-theme-options'),
-				'subtitle'		=> __( 'Please enter in any custom javascript you wish to add to the head of your pages. Requires opening and closing script tags.', 'lift-theme-options' ),
 				'mode'     => 'javascript',
 				'class' => 'lift-theme-admin-cssjs',
 				'theme'    => 'monokai',
