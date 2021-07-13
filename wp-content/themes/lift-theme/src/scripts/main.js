a6d57c69431ff58aa6dcc52b2a862e018c59945a
Nguyen Pham <baonguyenyam@gmail.com>
2021-07-13 11:31:51 -0500

    2.4.0

--- wp-content/themes/lift-theme/src/scripts/main.js
+++ wp-content/themes/lift-theme/src/scripts/main.js
@@ -1,16 +1,17 @@
-function lift_fixed_header () {
-	var getHeader = $('header#header.fixed-top').outerHeight(true)
-	if(getHeader) {
-		$('html').css({
-			'padding-top': getHeader+'px'
-		})
-	}
-}
+// function lift_fixed_header () {
+// 	var getHeader = $('header#header.fixed-top').outerHeight(true)
+// 	if(getHeader) {
+// 		$('html').css({
+// 			'padding-top': getHeader+'px'
+// 		})
+// 	}
+// }
 
-lift_fixed_header()
-LIFTReady(() => {
-	lift_fixed_header()
-});
-liftDOMChange(() => {
-});
+// lift_fixed_header()
+// LIFTReady(() => {
+// 	lift_fixed_header()
+// });
+// liftDOMChange(() => {
+// });
+
 
