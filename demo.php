6f2ff7aec4e81ecae0a48712ea91b95c3c56569e
Nguyen Pham <baonguyenyam@gmail.com>
2021-08-25 12:24:21 -0500

    update img

--- demo.php
+++ demo.php
@@ -1,11 +1,17 @@
 <?php 
 
-// $homepage = json_decode(file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/block.json'));
+// $homepage = json_decode(file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/demo.json'));
+
+// preg_match_all('#"(.*?)"#', $homepage, $match);
+// var_dump($match);
+
 // foreach ($homepage->data as &$value) {
-//     echo '<a href="'.$value->banner.'">'.$value->banner.'</a><br>';
+//     // var_dump($value->rawData[0]->columns);
+//     var_dump($value->rawData[0]->columns[0]->addons[0]->settings->upload_image);
+//     // echo '<a href="'.$value->banner.'">'.$value->banner.'</a><br>';
 // }
 
-$homepage = file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout.json');
+$homepage = file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/demo.json');
 preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $homepage, $match);
 
 foreach ($match[0] as &$value) {
