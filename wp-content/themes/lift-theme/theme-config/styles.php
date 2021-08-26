96e66d8f09e86d31aa288601e50624b5123a6968
Nguyen Pham <baonguyenyam@gmail.com>
2021-08-26 10:38:53 -0500

    done

--- wp-content/themes/lift-theme/theme-config/styles.php
+++ wp-content/themes/lift-theme/theme-config/styles.php
@@ -28,6 +28,8 @@ function lift_styles() {
 	if(isset($layout_style) && ($layout_style === '1' || $layout_style == 1) && $layout_size) {
 		$layout_css .= "#content.lift-content .wpb_wrapper{max-width: ".$layout_size_value."px; margin: 0 auto; width: 100%}";
 		$layout_css .= "#content.lift-content .container-fluid{max-width: ".$layout_size_value."px; margin: 0 auto; width: 100%}";
+		// THIS ONE APPLY FOR WOW PAGE BUILDER
+		$layout_css .= "#content.lift-content .content-wrapper .lift-main #wppb-builder-container .wppb-row-parent .wppb-container{max-width: ".$layout_size_value."px; margin: 0 auto; width: 100%}";
 		// TODO: layout post detail and archive page 
 	}
 	if(isset($layout_scrollbar) && ($layout_scrollbar === '1' || $layout_scrollbar == 1)) {
