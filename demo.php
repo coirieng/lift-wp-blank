e06e1a2b4810df366701169dd3826738a5137dff
Nguyen Pham <baonguyenyam@gmail.com>
2021-08-26 11:14:06 -0500

    update json

--- demo.php
+++ demo.php
@@ -11,8 +11,24 @@
 //     // echo '<a href="'.$value->banner.'">'.$value->banner.'</a><br>';
 // }
 
-$homepage = file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/demo.json');
-preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $homepage, $match);
+$homepage = file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-1.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-2.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-3.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-4.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-5.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-6.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-7.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-8.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-9.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-10.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-11.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-12.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-13.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-14.json');
+$homepage .= file_get_contents('http://demo.local:8888/wp-content/plugins/lift-pagebuilder/jsondata/layout/layout-15.json');
+preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', str_replace("\/", "/", $homepage), $match);
+
+// var_dump(str_replace("\/", "/", $homepage));
 
 foreach ($match[0] as &$value) {
     // echo $value . '<br>';
