2edeb47760fcd65d97c3341dbe75d168c7bce614
Nguyen Pham <baonguyenyam@gmail.com>
2021-09-01 15:26:35 -0500

    DOCUMENTS

---
+++ wp-content/themes/wp-wow-theme/@dev/devdoc.md
@@ -0,0 +1,53 @@
+# FRONT END 
+
+## Layout 
+themes/wp-wow-theme/template-parts
+
+## Thumbnail 
+<!-- Add size -->
+<!-- Get all image size -->
+
+wp-wow-theme/theme-config/config/post-thumbnail.php
+has_post_thumbnail()
+the_post_thumbnail();
+the_post_thumbnail( 'your-custom-size' );
+set_post_thumbnail_size( 50, 50, true ); 
+
+## Action
+add_action( 'init', 'wow_add_image_size' );
+add_action( 'after_setup_theme', 'wow_get_all_image_sizes' );
+
+## Postype & Template 
+<!-- Add Thumbnail Column -->
+<!-- Add Template to Edit/Add -->
+wp-wow-theme/theme-config/config/admin.php
+
+## Breadcrumb 
+wp-wow-theme/theme-config/config/breadcrumb.php
+
+## Admin Custuomize theme 
+wp-wow-theme/theme-config/config/customize-tab.php
+
+## Style for FrontEnd 
+wp-wow-theme/theme-config/config/styles.php
+
+## Script for FrontEnd 
+wp-wow-theme/theme-config/config/scripts.php
+
+## Theme menu & Widgets
+wp-wow-theme/theme-config/config/setup.php
+
+## Theme sidebar & Footer sidebar 
+wp-wow-theme/theme-config/config/sidebar.php
+
+=======================================================================
+# ADMIN THEME
+All in one here
+themes/wp-wow-theme/theme-options/options
+themes/wp-wow-theme/theme-options/tabs
+
+## Required Plugins 
+wp-wow-theme/theme-options/required/list.php
+
+## Dev 
+wp-wow-theme/inc
--- wp-content/themes/wp-wow-theme/theme-config/config/post-thumbnail.php
+++ wp-content/themes/wp-wow-theme/theme-config/config/post-thumbnail.php
@@ -1,26 +1,30 @@
 <?php
 // Add image size for post thumbnail
 function wow_add_image_size() {
-    add_image_size( 'custom-size', 770, 350, true );
+    add_image_size( 'wow-big', 1200, 680, true );
+    add_image_size( 'wow-large', 960, 500, true );
+    add_image_size( 'wow-medium', 720, 440, true );
+    add_image_size( 'wow-small', 600, 360, true );
+    add_image_size( 'wow-mini', 400, 260, true );
+    add_image_size( 'wow-square', 200, 200, true );
 }
 
 function wow_get_all_image_sizes() {
     global $_wp_additional_image_sizes;
 
-    $default_image_sizes = get_intermediate_image_sizes();
+    $thumbnail_sizes = array();
 
-    foreach ( $default_image_sizes as $size ) {
-        $image_sizes[ $size ][ 'width' ] = intval( get_option( "{$size}_size_w" ) );
-        $image_sizes[ $size ][ 'height' ] = intval( get_option( "{$size}_size_h" ) );
-        $image_sizes[ $size ][ 'crop' ] = get_option( "{$size}_crop" ) ? get_option( "{$size}_crop" ) : false;
+    foreach ( get_intermediate_image_sizes() as $size ) {
+        $thumbnail_sizes[ $size ]['label'] = $size;
+        if ( in_array( $size, array( 'thumbnail', 'medium', 'medium_large', 'large' ) ) ) {
+            $thumbnail_sizes[ $size ]['width']  = (int) get_option( $size . '_size_w' );
+            $thumbnail_sizes[ $size ]['height'] = (int) get_option( $size . '_size_h' );
+            $thumbnail_sizes[ $size ]['crop']   = ( 'thumbnail' == $size ) ? (bool) get_option( 'thumbnail_crop' ) : false;
+        } elseif ( ! empty( $_wp_additional_image_sizes ) && ! empty( $_wp_additional_image_sizes[ $size ] ) ) {
+            $thumbnail_sizes[ $size ]['width']  = (int) $_wp_additional_image_sizes[ $size ]['width'];
+            $thumbnail_sizes[ $size ]['height'] = (int) $_wp_additional_image_sizes[ $size ]['height'];
+            $thumbnail_sizes[ $size ]['crop']   = (bool) $_wp_additional_image_sizes[ $size ]['crop'];
+        }
     }
-
-    if ( isset( $_wp_additional_image_sizes ) && count( $_wp_additional_image_sizes ) ) {
-        $image_sizes = array_merge( $image_sizes, $_wp_additional_image_sizes );
-    }
-    return $image_sizes;
+    return $thumbnail_sizes;
 }
-
-add_action('init', 'wow_add_image_size');
-
-var_dump(wow_get_all_image_sizes());
